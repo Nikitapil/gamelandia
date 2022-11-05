8eee5f1f1960941c22fce1e283a0b332a6bcc7a6
Nikitapil <nikitapiliavets@gmail.com>
2022-11-05 17:21:23 +0300

    create not found page

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import styles from '../styles/notfound.module.scss';
+import { useTitle } from '../hooks/useTitle';
+
+export const NotFound = () => {
+  useTitle('404');
+  const { t } = useTranslation();
+
+  return (
+    <div className="container">
+      <div className={styles.page}>
+        <div className={styles.numbers}>404</div>
+        <div className={styles.info}>
+          <h2 className={styles.title}>{t('sorry')}</h2>
+          <p className={styles.text}>{t('page_not_found')}</p>
+          <Link className={styles.link} to="/">
+            {t('go_to_main')}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
