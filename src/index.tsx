b626cdbfda67c22a6929b206eb3933c8586112d4
Nikitapil <nikitapiliavets@gmail.com>
2023-04-26 16:41:39 +0300

    configure new store

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Suspense } from 'react';
-import { store } from './redux/store';
 import App from './App';
 import { RoundLoader } from './components/UI/Loaders/RoundLoader';
 import './i18n';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
