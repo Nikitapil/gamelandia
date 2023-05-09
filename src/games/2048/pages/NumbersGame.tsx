37426463d1ca0169d10a3feb17c9c8a8e6cce9da
Nikitapil <nikitapiliavets@gmail.com>
2023-05-09 19:43:06 +0300

    refactor 2048 page

--- src/games/2048/pages/NumbersGame.tsx
+++ src/games/2048/pages/NumbersGame.tsx
@@ -6,20 +6,20 @@ import { useBreadcrumbs } from '../../../app/hooks/useBreadcrumbs';
 import { breadcrumbs } from '../../../constants/breadcrumbs';
 import { useAppSelector } from '../../../hooks/useAppSelector';
 import { authSelector } from '../../../store/selectors';
-import { CommonScoreBoard } from '../../../score/components/CommonScoreBoard';
 import { EGamesNames } from '../../constants';
+import { GameWithScore } from '../../components/GameWithScore/GameWithScore';
 
 export const NumbersGame = () => {
   useTitle('2048');
   useBreadcrumbs([breadcrumbs.main, breadcrumbs['2048']]);
   const { user } = useAppSelector(authSelector);
+
   return (
     <div className={`container ${styles.page}`}>
       <h1 className="page-title">2048 Game</h1>
-      <div className={styles['numbers-boards']}>
+      <GameWithScore game={EGamesNames.NUMBERS} user={user}>
         <NumbersBoard />
-        <CommonScoreBoard user={user} game={EGamesNames.NUMBERS} />
-      </div>
+      </GameWithScore>
     </div>
   );
 };
---
+++ src/games/components/GameWithScore/GameWithScore.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { EGamesNames } from '../../constants';
+import { IUser } from '../../../auth/types';
+import styles from './game-with-score.module.scss';
+import { CommonScoreBoard } from '../../../score/components/CommonScoreBoard';
+
+interface IGameWithScoreProps {
+  game: EGamesNames;
+  user: IUser | null;
+  children: ReactNode;
+}
+
+export const GameWithScore = ({
+  game,
+  user,
+  children
+}: IGameWithScoreProps) => {
+  return (
+    <div className={styles.boards}>
+      <div>{children}</div>
+      <CommonScoreBoard game={game} user={user} />
+    </div>
+  );
+};
