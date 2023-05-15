41b6d060c785a013fe60d78aaa7220c2648bfee1
Nikitapil <nikitapiliavets@gmail.com>
2023-05-15 23:20:12 +0300

    refactor chess timer modal

--- src/games/chess/components/TimerModal.tsx
+++ src/games/chess/components/TimerModal.tsx
@@ -1,16 +1,19 @@
-import React, { memo, useState } from 'react';
+import { ChangeEvent, memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AppButton } from '../../../components/UI/AppButton/AppButton';
+import styles from '../assets/styles/chess.module.scss';
 
-interface TimerModalProps {
+interface ITimerModalProps {
   start: (time: number) => void;
   closeModal: () => void;
 }
 
-export const TimerModal = memo(({ start, closeModal }: TimerModalProps) => {
-  const [time, setTime] = useState(60);
+export const TimerModal = memo(({ start, closeModal }: ITimerModalProps) => {
   const { t } = useTranslation();
-  const timeChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  const [time, setTime] = useState(60);
+
+  const timeChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setTime(+value);
   };
@@ -24,19 +27,19 @@ export const TimerModal = memo(({ start, closeModal }: TimerModalProps) => {
 
   return (
     <div
-      className="timer-modal"
+      className={styles['timer-modal']}
       data-testid="timer-modal"
     >
       <label htmlFor="time-input">
         <input
           type="number"
-          className="timer-input"
+          className={styles['timer-input']}
           placeholder="Time"
           value={time}
           onChange={timeChangeHandler}
           data-testid="time-input"
           id="time-input"
-        />{' '}
+        />
         {t('min')}
       </label>
       <AppButton
