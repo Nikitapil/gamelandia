f225bc3347762ea6cf8c389a0f02272d7c0935cd
Nikitapil <nikitapiliavets@gmail.com>
2023-05-22 22:19:32 +0300

    finish clone invaders refactoring

--- src/games/clone-invaders/pages/CloneInvaders.tsx
+++ src/games/clone-invaders/pages/CloneInvaders.tsx
@@ -3,23 +3,26 @@ import { InvadersField } from '../components/InvadersField';
 import { breadcrumbs } from '../../../constants/breadcrumbs';
 import { useBreadcrumbs } from '../../../app/hooks/useBreadcrumbs';
 import { useTitle } from '../../../hooks/useTitle';
-import invadersStyles from '../assets/styles/invaders.module.scss';
-import { CommonScoreBoard } from '../../../score/components/CommonScoreBoard';
+import styles from '../assets/styles/invaders.module.scss';
 import { useAppSelector } from '../../../hooks/useAppSelector';
 import { authSelector } from '../../../store/selectors';
 import { EGamesNames } from '../../constants';
+import { GameWithScore } from '../../components/GameWithScore/GameWithScore';
 
 export const CloneInvaders = () => {
   useTitle('Clone Invaders');
   useBreadcrumbs([breadcrumbs.main, breadcrumbs.cloneInvaders]);
   const { user } = useAppSelector(authSelector);
+
   return (
-    <div className={`container ${invadersStyles.invaders}`}>
+    <div className={`container ${styles.invaders}`}>
       <h2 className="page-title">Clone invaders</h2>
-      <div className={invadersStyles.boards}>
+      <GameWithScore
+        game={EGamesNames.CLONE_INVADERS}
+        user={user}
+      >
         <InvadersField />
-        <CommonScoreBoard user={user} game={EGamesNames.CLONE_INVADERS} />
-      </div>
+      </GameWithScore>
     </div>
   );
 };
