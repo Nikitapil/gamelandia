be9578e7b27cd659c6078fd95467db1a8ff8ba80
Nikitapil <nikitapiliavets@gmail.com>
2024-04-05 23:17:10 +0300

    add score to brick game

--- src/games/brick/pages/BrickGame.tsx
+++ src/games/brick/pages/BrickGame.tsx
@@ -2,27 +2,48 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from '../assets/styles/styles.module.scss';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../constants';
 import { BrickGameModel } from '../models/BrickGame';
+import { GameWithScore } from '../../components/GameWithScore/GameWithScore';
+import { EGamesNames } from '../../constants';
+import { authSelector } from '../../../store/selectors';
+import { useCreateScore } from '../../../score/hooks/useCreateScore';
+import { useAppSelector } from '../../../hooks/useAppSelector';
 
 const BrickGame = () => {
   const [game, setGame] = useState<BrickGameModel | null>(null);
-  const [score, setScore] = useState<number>(0);
-  const [lives, setLives] = useState<number>(0);
+  const [score, setScore] = useState(0);
+  const [lives, setLives] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const gameRef = useRef<HTMLDivElement>(null);
 
-  const updateGame = (gameInstance: BrickGameModel) => {
+  const { user } = useAppSelector(authSelector);
+  const createScore = useCreateScore();
+
+  const updateGame = useCallback((gameInstance: BrickGameModel) => {
     setScore(gameInstance.score);
     setLives(gameInstance.lives);
-  };
-
-  const newGame = useCallback(() => {
-    setGame(() => (canvasRef.current ? new BrickGameModel(canvasRef.current, updateGame) : null));
+    if (gameInstance.isGameOver) {
+      setIsGameOver(true);
+    }
   }, []);
 
+  const newGame = useCallback(() => {
+    setIsGameOver(false);
+    setGame(() => (canvasRef.current ? new BrickGameModel(canvasRef.current, updateGame) : null));
+  }, [updateGame]);
+
   useEffect(() => {
-    newGame();
-    gameRef.current?.focus();
-  }, [newGame]);
+    if (!game) {
+      newGame();
+      gameRef.current?.focus();
+    }
+    if (isGameOver && user) {
+      createScore({
+        gameName: EGamesNames.BRICK,
+        value: score
+      });
+    }
+  }, [createScore, game, isGameOver, newGame, score, user]);
 
   const keyDownHandler = useCallback(
     (e: React.KeyboardEvent) => {
@@ -55,26 +76,31 @@ const BrickGame = () => {
   );
 
   return (
-    <div
-      onKeyDown={keyDownHandler}
-      onKeyUp={keyUpHandler}
-      tabIndex={0}
-      ref={gameRef}
+    <GameWithScore
+      game={EGamesNames.BRICK}
+      user={user}
     >
-      <div className={`container ${styles.brick}`}>
-        <h2 className="page-title">Brick Game</h2>
-        <div className={styles.meta}>
-          <p>Score: {score}</p>
-          <p>Lives: {lives}</p>
+      <div
+        onKeyDown={keyDownHandler}
+        onKeyUp={keyUpHandler}
+        tabIndex={0}
+        ref={gameRef}
+      >
+        <div className={`container ${styles.brick}`}>
+          <h2 className="page-title">Brick Game</h2>
+          <div className={styles.meta}>
+            <p>Score: {score}</p>
+            <p>Lives: {lives}</p>
+          </div>
+          <canvas
+            ref={canvasRef}
+            className={styles.canvas}
+            width={CANVAS_WIDTH}
+            height={CANVAS_HEIGHT}
+          />
         </div>
-        <canvas
-          ref={canvasRef}
-          className={styles.canvas}
-          width={CANVAS_WIDTH}
-          height={CANVAS_HEIGHT}
-        />
       </div>
-    </div>
+    </GameWithScore>
   );
 };
 
