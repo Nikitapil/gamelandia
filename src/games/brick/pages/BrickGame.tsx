0da36ae58ae631740a23a5f89cca434975d1d3bf
Nikitapil <nikitapiliavets@gmail.com>
2024-04-06 21:29:01 +0300

    add i18n for brick game meta info

--- src/games/brick/pages/BrickGame.tsx
+++ src/games/brick/pages/BrickGame.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from '../assets/styles/styles.module.scss';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../constants';
 import { BrickGameModel } from '../models/BrickGame';
@@ -9,6 +10,7 @@ import { useCreateScore } from '../../../score/hooks/useCreateScore';
 import { useAppSelector } from '../../../hooks/useAppSelector';
 
 const BrickGame = () => {
+  const { t } = useTranslation();
   const [game, setGame] = useState<BrickGameModel | null>(null);
   const [score, setScore] = useState(0);
   const [lives, setLives] = useState(0);
@@ -87,8 +89,12 @@ const BrickGame = () => {
         <div className={`container ${styles.brick}`}>
           <h2 className="page-title">Brick Game</h2>
           <div className={styles.meta}>
-            <p>Score: {score}</p>
-            <p>Lives: {lives}</p>
+            <p>
+              {t('score')}: {score}
+            </p>
+            <p>
+              {t('lives')}: {lives}
+            </p>
           </div>
           <canvas
             ref={canvasRef}
